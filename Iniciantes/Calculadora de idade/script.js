e7633f9be87bc0de8a9bb7cb1a8e32a8d5df824d
cristiandoring <cristiandoring@gmail.com>
2024-11-20 17:06:48 -0300

    Leap year test

--- Iniciantes/Calculadora de idade/script.js
+++ Iniciantes/Calculadora de idade/script.js
@@ -1,4 +1,6 @@
 function calculate(){
+
+    let response
     //armazena o valor retirado do input (formato String)
     const dateValue = document.getElementById('date').value
 
@@ -18,6 +20,34 @@ function calculate(){
     const currentMonth = currentDayOfYear.getMonth() + 1
     const currentDay = currentDayOfYear.getDate()
 
+    //calcula as diferenças
+    let years = currentYear - year
+    let months = currentMonth - month
+    let days = currentDay - day
+
+    //testa se a data de nascimento é maior que a atual
+    if(years < 0){
+        document.getElementById("response").innerHTML = `Digite uma data válida`
+        document.getElementById("response").style.color = "red"
+    }
+
+    //teste de ano não bissexto para garantir que não tenha datas 30 e 31 em fev
+    if (currentMonth == 2){
+        if ((currentYear % 4 == 0 && currentYear % 100 != 0) || (currentYear % 400 == 0)){
+            if(currentDay == 30 || currentDay == 31){
+                document.getElementById("response").innerHTML = `Digite uma data válida`
+                document.getElementById("response").style.color = "red"
+            }
+        }
+        else{
+            if(currentDay == 29 || currentDay == 30 || currentDay == 31){
+                document.getElementById("response").innerHTML = `Digite uma data válida`
+                document.getElementById("response").style.color = "red"
+            }
+        }
+    }
     
-      return document.write(currentDay)
+    response = document.getElementById("response").innerHTML = `You have ${years} years, ${months} months, and ${days} days old.`;
+
+
 }
